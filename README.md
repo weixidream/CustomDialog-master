3890e91e8778b7b7b6cc037febbdf87ffb8f3ec6
Liuhc <771346371@qq.com>
2016-05-19 15:28:57 +0800

    文档修改，增加使用说明

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CustomDialog-master
-#测试上传
+一个简单的自定义Dialog
+包括dialog内view的设置
+
+用法如下
+DialogBuilder.getInstance(MainActivity.this)
+                        .withTitle("Dialog /Title")
+                        .withContent("Dialog /Content")
+                        .withConfirmText("Button OK")
+                        .setConfirmClickListener(new DialogBuilder.OnClickListener() {
+                            @Override
+                            public void onClick(DialogBuilder dialogBuilder) {
+                                Toast.makeText(MainActivity.this, "Dialog Ok Button", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+
+Gradle 配置如下
+dependencies {
+    compile 'com.liuhc.library.dialog:DialogLibrary:1.0.2'
+}
--- app/src/main/java/com/liuhc/sample/MainActivity.java
+++ app/src/main/java/com/liuhc/sample/MainActivity.java
@@ -3,6 +3,7 @@ package com.liuhc.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.liuhc.library.dialog.DialogBuilder;
 
@@ -18,8 +19,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_show:
-
-                DialogBuilder.getInstance(MainActivity.this).show();
+                DialogBuilder.getInstance(MainActivity.this)
+                        .withTitle("Dialog /Title")
+                        .withContent("Dialog /Content")
+                        .withConfirmText("Button OK")
+                        .setConfirmClickListener(new DialogBuilder.OnClickListener() {
+                            @Override
+                            public void onClick(DialogBuilder dialogBuilder) {
+                                Toast.makeText(MainActivity.this, "Dialog Ok Button", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
                 break;
         }
     }
